a9848aba995e70543e5f5922ba20ebbbf603b556
Masha-Korol <markor2000@inbox.ru>
2020-03-31 19:30:31 +0300

    Update ClientController.java

--- src/main/java/com/example/stavki/entities/controllers/ClientController.java
+++ src/main/java/com/example/stavki/entities/controllers/ClientController.java
@@ -1,4 +1,5 @@
 package com.example.stavki.entities.controllers;
+//oooooooooo****
 
 import java.util.List;
 
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 class ClientController {
 
